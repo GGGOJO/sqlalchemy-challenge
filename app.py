7c779d2c3ad0d3a2c74c228a71438ddecab9d3af
Gigi Jones <72949687+GGGOJO@users.noreply.github.com>
2021-02-27 16:03:19 -0800

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def welcome():
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
-        f"/ api/v1.0/temp/start<br/>"
+        f"/api/v1.0/temp/start<br/>"
         f"/api/v1.0/temp/start/end"
     )
 
